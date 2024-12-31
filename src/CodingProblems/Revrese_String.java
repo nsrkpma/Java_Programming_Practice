52a22d0c7fe7c5c7361e6be563f3cca478be3198
Nirmal <nsr.kpma@gmail.com>
2024-12-31 16:37:33 +0530

    added java programs

---
+++ src/CodingProblems/Revrese_String.java
@@ -0,0 +1,28 @@
+//Write a program to reverse each word in a String
+package CodingProblems;
+
+import java.util.Scanner;
+
+public class Revrese_String {
+	
+	public static String reverseString(String str) {
+		String[] words=str.split(" ");
+		StringBuilder reversedString=new StringBuilder();
+		for(String word : words) {
+			StringBuilder reversedword=new StringBuilder(word);
+			reversedString.append(reversedword.reverse()).append(" ");
+		}
+		return reversedString.toString();
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Scanner myObj = new Scanner(System.in);
+		 String userinput=myObj.nextLine();
+		System.out.println(reverseString(userinput));
+		
+
+	}
+
+}
