766fcde099b883806152364b217c3c2b7b36502d
Nirmal <nsr.kpma@gmail.com>
2025-01-23 19:15:20 +0530

    added more practice

--- src/CodingProblems/Practice/PracticeCodingProblems.java
+++ src/CodingProblems/Practice/PracticeCodingProblems.java
@@ -4,12 +4,28 @@ public class PracticeCodingProblems {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int[] arr = { 2, 3, 4, 5, 6, 7, 8 };
+		int[] arr = { 2, 3, 4, 5, 6, 7, 8 ,20};
 		System.out.println(sumArrayElements(arr));
 		System.out.println(multiply(3, 9));
+		System.out.println(multiplyWhile(3, 9));
+		System.out.println(maxDifference(arr));
 
 	}
-
+	
+	//Find the maximum difference between adjacent array elements
+	public static int maxDifference(int[]arr) {
+		int diff=0;
+		int maxdiff=0;;
+		for(int i=0;i<arr.length-1;i++) {
+			diff=Math.abs((arr[i]-arr[i+1]));
+			if(diff>maxdiff) {
+				maxdiff=diff;
+			}
+		}
+		return maxdiff;
+	}
+	
+	//Find the sum of an integer array elements
 	public static int sumArrayElements(int[] arr) {
 		int sum = 0;
 		for (int i = 0; i < arr.length; i++) {
@@ -17,12 +33,25 @@ public class PracticeCodingProblems {
 		}
 		return sum;
 	}
-
+	
+	//Multiply two numbers without using * operator using for loop
 	public static int multiply(int a, int b) {
 		int result = a;
 		for (int i = 1; i < b; i++) {
 			result += a;
 		}
 		return result;
+	}
+	
+	//Multiply two numbers without using * operator using while loop
+	public static int multiplyWhile(int a,int b) {
+		int result=0;
+		int i=1;
+		while(i<=b) {
+			result+=a;
+			i++;
+		}
+		return result;
+		
 	}
 }
